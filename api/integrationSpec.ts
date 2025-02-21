c6ea511f44fde794d06bb6654de73f5688c067ca
Joeonome <joseph.onumeguolor@gmail.com>
2025-02-21 23:45:13 +0100

    Chore: removed settings from integrationspec

--- api/integrationSpec.ts
+++ api/integrationSpec.ts
@@ -23,48 +23,8 @@ export const integrationSpec = {
     ],
     "author": "Joeonome",
     "permissions": {},
-      "settings": [
-        {
-          "label": "Gender",
-          "type": "radio",
-          "required": true,
-          "default": "Female",
-          "options": ["Male", "Female"]
-        },
-        {
-          "label": "Key",
-          "type": "text",
-          "required": true,
-          "default": "1234567890"
-        },
-        {
-          "label": "Do you want to continue",
-          "type": "checkbox",
-          "required": true,
-          "default": "Yes"
-        },
-        {
-          "label": "Provide Speed",
-          "type": "number",
-          "required": true,
-          "default": "1000"
-        },
-        {
-          "label": "Sensitivity Level",
-          "type": "dropdown",
-          "required": true,
-          "default": "Low",
-          "options": ["High", "Low"]
-        },
-        {
-          "label": "Alert Admin",
-          "type": "multi-checkbox",
-          "required": true,
-          "default": "Super-Admin",
-          "options": ["Super-Admin", "Admin", "Manager", "Developer"]
-        }
-      ],
+      "settings": [],
       "target_url": "https://hng12-stage3-telex-integration-bible-lookup-app.vercel.app/bible",
       "tick_url": "https://hng12-stage3-telex-integration-bible-lookup-app.vercel.app/tick_url"
     }
-  } 
+  }
