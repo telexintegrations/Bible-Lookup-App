51f993c8ef16540c91c67a4e0de33fd9f96187a5
Joeonome <joseph.onumeguolor@gmail.com>
2025-02-22 04:21:44 +0100

    chore: added app image url & updated description

--- api/integrationSpec.ts
+++ api/integrationSpec.ts
@@ -5,8 +5,8 @@ export const integrationSpec = {
         "updated_at": "2025-02-22"
     }, 
     "descriptions": {
-        "app_description": "Automatically replaces Bible references with their passages/texts. \n Example format: Genesis 1:1, 1 John 2:1-3",
-        "app_logo": "https://ibb.co/whd1VMVd",
+        "app_description": "Automatically replaces Bible references with their passages/texts"
+        "app_logo": "https://cdn2.iconfinder.com/data/icons/bible-books/154/search-for-text-bible-512.png",
         "app_name": "Bible Lookup",
         "app_url": "https://hng12-stage3-telex-integration-bible-lookup-app.vercel.app",
         "background_color": "#000000"
@@ -19,7 +19,10 @@ export const integrationSpec = {
     "key_features": [
         "Bible Search",
         "Bible Replace",
-        "KJV"
+        "KJV",
+        {"Example format": [ "Genesis 1:1", 
+        "1 John 2:1-3"
+        ]}
     ],
     "author": "Joeonome",
     "permissions": {},
